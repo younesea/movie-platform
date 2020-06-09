aee1ba2572df8483d5bd0da0f017084119977af2
younesea <younes@yea.solutions>
2020-06-09 20:58:17 +0200

    Adds a helper function and refactors MovieBox to use that function

--- components/MovieBox/MovieBox.spec.ts
+++ components/MovieBox/MovieBox.spec.ts
@@ -1,40 +1,47 @@
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { MountOptions, RouterLinkStub } from "@vue/test-utils";
 import MovieBox from '@/components/MovieBox'
-import { LoadingAttr } from "@/enums";
+import { LoadingAttr } from '@/enums'
+import { mountComponent } from '~/utils/tests'
+import Vue from "vue";
 
-interface IPropsData {
-  name: string;
-  src: string;
-  loading: string;
+interface IConfig extends MountOptions<Vue> {
+  propsData: {
+    name: string
+    src: string
+    loading: string
+  }
 }
 
 describe('MovieBox', () => {
-  const defaultPropsData = {
-    name: 'The Blackout',
-    src: 'https://image.tmdb.org/t/p/original/avl9MEQhtvokNnzoWepkmHBZ2ss.jpg',
-    loading: LoadingAttr.Lazy
-  }
-
-  const createWrapper = (propsData: IPropsData = defaultPropsData) => mount(MovieBox, {
-    propsData,
+  const defaultConfig: IConfig = {
+    propsData: {
+      name: 'The Blackout',
+      src:
+        'https://image.tmdb.org/t/p/original/avl9MEQhtvokNnzoWepkmHBZ2ss.jpg',
+      loading: LoadingAttr.Lazy
+    },
     stubs: {
       NuxtLink: RouterLinkStub
     }
-  })
-
-  const defaultWrapper = createWrapper()
+  }
+  const createWrapper = mountComponent(MovieBox)
+  const defaultWrapper = createWrapper(defaultConfig)
 
   it('has an image', () => {
+    const { propsData } = defaultConfig
+
     expect(defaultWrapper.findAll('img').length).toBe(1)
-    expect(defaultWrapper.find('img').attributes('src')).toBe(defaultPropsData.src)
-    expect(defaultWrapper.find('img').attributes('alt')).toBe(defaultPropsData.name)
-    expect(defaultWrapper.find('img').attributes('loading')).toBe(defaultPropsData.loading)
+    expect(defaultWrapper.find('img').attributes('src')).toBe(propsData.src)
+    expect(defaultWrapper.find('img').attributes('alt')).toBe(propsData.name)
+    expect(defaultWrapper.find('img').attributes('loading')).toBe(
+      propsData.loading
+    )
   })
 
   it('sets the loading attribute on auto when not specified', () => {
-    const propsData = {...defaultPropsData}
+    const propsData = { ...defaultConfig.propsData }
     delete propsData.loading
-    const wrapper = createWrapper(propsData)
+    const wrapper = createWrapper({ ...defaultConfig, ...{ propsData } })
 
     expect(wrapper.find('img').attributes('loading')).toBe(LoadingAttr.Auto)
   })
---
+++ utils/tests.ts
@@ -0,0 +1,8 @@
+import Vue, { VueConstructor } from 'vue'
+import { mount, MountOptions } from "@vue/test-utils";
+
+export const mountComponent = (component: VueConstructor<Vue>) => (
+  config: MountOptions<Vue>
+) => {
+  return mount(component, config)
+}
